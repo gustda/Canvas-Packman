5028afd8fd95c5de4179e23e6017949ea998061d
gustda <d.gustorff@gmail.com>
2018-01-21 16:16:15 +0100

    Packman reagiert auf Pfeiltasten

---
+++ Canvas-Packman/Packman.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Canvas_Packman
+{
+    class Packman
+    {
+      public   int X { get; private set; }
+       public  int Y { get; private set; }
+        public int Size { get; private  set; }
+
+        public int Step { get; private set; }
+        
+
+        public Packman()
+        {
+            Size = 20;
+            Step = 10;
+            X = 200;
+            Y=250;
+        }
+
+        public void ProcessKey (Keys key)
+        {
+            // handle Packman Steps
+            switch(key)
+            {
+                case Keys.Down:
+                    if (Y < 600 - Step-Size)
+                        Y = Y + Step;
+                    break;
+                case Keys.Up:
+                    if (Y > 0 + Step+Size)
+                        Y = Y - Step;
+                    break;                    
+                case Keys.Left:
+                    if (X > 0 + Step+ Size)
+                        X = X - Step;
+                    break;
+                case Keys.Right:
+                    if (X < 600 - Step- Size)
+                        X = X + Step;
+                    break;
+            }
+        }
+    }
+}
---
+++ Canvas-Packman/PackmanLauncher.cs
@@ -0,0 +1,49 @@
+﻿using csharp_canvas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Canvas_Packman
+{
+   
+
+    class PackmanLauncher
+    {
+        CsharpCanvas canvas;
+
+        Packman packman = new Packman();
+
+        public PackmanLauncher()
+        {
+            canvas = new CsharpCanvas();
+
+            canvas.Draw += Canvas_Draw;
+
+            // für kontinuierliches Zeichnen
+            canvas.Setup(30);            
+        }
+
+        private void Canvas_Draw()
+        {
+            CheckKeys();
+            DrawPackman();
+        }
+
+        private void DrawPackman()
+        {
+            canvas.SetForegroundColor(System.Drawing.Color.Yellow);
+            canvas.AddCircle(packman.X, packman.Y, packman.Size, Fill.Fill  );
+        }
+
+        private void CheckKeys()
+        {
+            if(canvas.LastPressedKey!= System.Windows.Forms.Keys.None)
+            {
+                packman.ProcessKey(canvas.LastPressedKey);
+                canvas.KeyHandled();
+            }
+        }
+    }
+}
